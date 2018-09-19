71d7a65fbfb937762ab0456fe160be59c601eb6f
104021056 <104021056@live.asia.edu.tw>
2018-09-19 16:35:01 +0800

    計算

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("輸入一攝氏溫度");
+        System.out.println("輸入兩個整數");
         Scanner scn = new Scanner(System.in);
-        float C = scn.nextFloat();
-        float F = C*9/5+32;
-        System.out.println("華氏溫度"+ F);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = a+b;
+        int d = a-b;
+
+        System.out.println("相加"+" "+ c);
+        System.out.println("相乘"+" "+ a*b);
+        System.out.println("相減"+" "+ d);
     }
 }
